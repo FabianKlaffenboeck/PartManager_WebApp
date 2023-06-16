c06332acab9628682390d8b4bc4f253d8532cd73
Fabian <fabian@klaffenboeck.eu>
2023-06-16 17:17:25 +0200

    switched from http to https

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080'
-  // apiUrl: 'http://part01db.klaffenboeck.eu'
+  // apiUrl: 'https://part01db.klaffenboeck.eu'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://part01db.klaffenboeck.eu'
+  apiUrl: 'https://part01db.klaffenboeck.eu'
 };
