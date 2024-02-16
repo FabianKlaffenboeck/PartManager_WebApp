4d3f64f50c451d7c2574f2db0e6816d8a0a5cadb
Fabian <fabian@klaffenboeck.eu>
2024-02-16 11:56:15 +0100

    added auto naming for shelfs

--- src/app/dialogs/shelf-dialog/shelf-dialog.component.ts
+++ src/app/dialogs/shelf-dialog/shelf-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
 import {TrayModel} from "../../../../service/models/Tray.model";
 import {ShelfModel} from "../../../../service/models/Shelf.model";
+import {ShelfService} from "../../../../service/data/Shelf.service";
 
 @Component({
   selector: 'app-part-dialog',
@@ -13,10 +14,14 @@ export class ShelfDialogComponent implements OnInit {
   name: any;
   count: any;
 
+  shelfs: ShelfModel[] = []
+
   constructor(
     public dialogRef: DynamicDialogRef,
-    public config: DynamicDialogConfig
+    public config: DynamicDialogConfig,
+    public shelfService: ShelfService,
   ) {
+    this.shelfService.get().subscribe(it => this.shelfs = it)
   }
 
   ngOnInit() {
@@ -33,6 +38,12 @@ export class ShelfDialogComponent implements OnInit {
 
     for (let i = 0; i < this.count; i++) {
       trays.push(new TrayModel({name: String(i).padStart(3, "0")}))
+    }
+
+    if (! this.name) {
+      let numAbbr: (num: number) => string;
+      numAbbr = (num: number) => num <= 0 ? '' : numAbbr(Math.floor((num - 1) / 26)) + String.fromCharCode((num - 1) % 26 + 65);
+      this.name = String(numAbbr(this.shelfs.length + 1)).padStart(4, "0")
     }
 
     let shelfModel: ShelfModel = {
