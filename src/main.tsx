6ef44c45df0cb893048045f8f17b2694e91c1708
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2025-05-06 21:10:51 +0200

    added basic layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Page from "@/page.tsx";
+import {ThemeProvider} from './components/theme-provider';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Page/>
+        </ThemeProvider>
+    </StrictMode>,
 )
