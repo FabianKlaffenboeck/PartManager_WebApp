5c0e0307c375794ddbb40e68f99a927b4d13770b
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2025-06-08 21:54:25 +0200

    basic skeleton finished

--- src/components/searchableSelect.tsx
+++ src/components/searchableSelect.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import * as React from "react"
+import {useState} from "react"
 import {CheckIcon, ChevronsUpDownIcon} from "lucide-react"
 
 import {cn} from "@/lib/utils"
@@ -11,47 +11,55 @@ import {Popover, PopoverContent, PopoverTrigger,} from "@/components/ui/popover"
 
 export type searchableSelectElement = { id: number, label: string }
 
-export function SearchableSelect({placeholder, elements}: {
+export function SearchableSelect({placeholder, elements, value, setValue}: {
     placeholder: string
     elements: searchableSelectElement[]
+    value?: number | undefined
+    setValue: (val: number|undefined) => void
 }) {
-    const [open, setOpen] = React.useState(false)
-    const [value, setValue] = React.useState<number>()
+    const [open, setOpen] = useState(false)
+    const [inputValue, setInputValue] = useState("")
 
-    return (<Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-            <Button
-                variant="outline"
-                role="combobox"
-                aria-expanded={open}
-                className="w-[200px] justify-between"
-            >
-                {value ? elements.find((framework) => framework.id == value)?.label : ("select a " + placeholder)}
-                <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50"/>
-            </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
-            <Command>
-                <CommandInput placeholder={("select a " + placeholder)}/>
-                <CommandList>
-                    <CommandEmpty>{"No " + placeholder + " found."}</CommandEmpty>
-                    <CommandGroup>
-                        {elements.map((element) => (
-                            <CommandItem
-                                key={element.id}
-                                value={element.id.toString()}
-                                onSelect={(currentValue) => {
-                                    setValue(currentValue === value?.toString() ? undefined : Number(currentValue))
-                                    setOpen(false)
-                                }}>
-                                <CheckIcon
-                                    className={cn("mr-2 h-4 w-4", value == element.id ? "opacity-100" : "opacity-0")}
-                                />
-                                {element.label}
-                            </CommandItem>))}
-                    </CommandGroup>
-                </CommandList>
-            </Command>
-        </PopoverContent>
-    </Popover>)
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[200px] justify-between"
+                >
+                    {value ? elements.find((framework) => framework.id == value)?.label : ("select a " + placeholder)}
+                    <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50"/>
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandInput
+                        value={inputValue}
+                        onValueChange={setInputValue}
+                        placeholder={("select a " + placeholder)}
+                    />
+                    <CommandList>
+                        <CommandEmpty>{"No " + placeholder + " found."}</CommandEmpty>
+                        <CommandGroup>
+                            {elements.filter(f => f.label.toLowerCase().includes(inputValue.toLowerCase())).map((element) => (
+                                <CommandItem
+                                    key={element.id}
+                                    value={element.id.toString()}
+                                    onSelect={(currentValue) => {
+                                        setValue(currentValue === value?.toString() ? undefined : Number(currentValue))
+                                        setOpen(false)
+                                    }}>
+                                    <CheckIcon
+                                        className={cn("mr-2 h-4 w-4", value == element.id ? "opacity-100" : "opacity-0")}
+                                    />
+                                    {element.label}
+                                </CommandItem>))}
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    )
 }
--- src/pages/Dialogs/CreateEditPart.tsx
+++ src/pages/Dialogs/CreateEditPart.tsx
@@ -32,6 +32,20 @@ export function CreateEditPart({open, part, cb}: {
     const [shelfs, setShelfs] = useState<Shelf[]>([]);
     const [electricalUnits, setElectricalUnits] = useState<ElectricalUnit[]>([]);
 
+
+    const [name, setName] = useState("");
+    const [value, setValue] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [electricalUnit, setElectricalUnit] = useState<number | undefined>();
+    const [footprint, setFootprint] = useState<number | undefined>();
+    const [partType, setPartType] = useState<number | undefined>();
+    const [manufacturer, setManufacturer] = useState<number | undefined>();
+    const [tray, setTray] = useState<number | undefined>();
+
+    function buildPart(): Part | undefined {
+        return part
+    }
+
     useEffect(() => {
         getFootprints()
             .then((data) => setFootprints(data))
@@ -62,6 +76,7 @@ export function CreateEditPart({open, part, cb}: {
             .catch((error) => console.error('Error:', error));
     }, []);
 
+
     return (
         <Sheet open={open}>
             <SheetContent>
@@ -78,19 +93,36 @@ export function CreateEditPart({open, part, cb}: {
                     {/*name: string*/}
                     <div className="grid gap-3">
                         <Label htmlFor="sheet-demo-name">Name</Label>
-                        <Input id="sheet-demo-name" defaultValue="Pedro Duarte"/>
+                        <Input
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                            id="sheet-demo-name"
+                            defaultValue="Pedro Duarte"
+                        />
                     </div>
 
                     {/*quantity: number*/}
                     <div className="grid gap-3">
                         <Label htmlFor="sheet-demo-name">Quantity</Label>
-                        <Input id="sheet-demo-name" defaultValue="Pedro Duarte"/>
+                        <Input
+                            type="number"
+                            value={quantity}
+                            onChange={e => setQuantity(e.target.value)}
+                            id="sheet-demo-name"
+                            defaultValue="Pedro Duarte"
+                        />
                     </div>
 
                     {/*value: number | null*/}
                     <div className="grid gap-3">
                         <Label htmlFor="sheet-demo-name">Value</Label>
-                        <Input id="sheet-demo-name" defaultValue="Pedro Duarte"/>
+                        <Input
+                            type="number"
+                            value={value}
+                            onChange={e => setValue(e.target.value)}
+                            id="sheet-demo-name"
+                            defaultValue="Pedro Duarte"
+                        />
                     </div>
 
                     {/*electricalUnit: ElectricalUnit | null*/}
@@ -98,10 +130,12 @@ export function CreateEditPart({open, part, cb}: {
                         <Label htmlFor="sheet-demo-username">ElectricalUnit</Label>
                         <SearchableSelect
                             placeholder={"ElectricalUnit"}
-                            elements={electricalUnits.map((it,index) => ({
+                            elements={electricalUnits.map((it, index) => ({
                                 id: index,
                                 label: it.toString(),
                             }))}
+                            value={electricalUnit}
+                            setValue={setElectricalUnit}
                         ></SearchableSelect>
                     </div>
 
@@ -110,8 +144,10 @@ export function CreateEditPart({open, part, cb}: {
                         <Label htmlFor="sheet-demo-username">Footprint</Label>
                         <SearchableSelect
                             placeholder={"Footprint"}
-                            elements={footprints.map(({id, metric}) => ({id: id, label: metric}))}>
-                        </SearchableSelect>
+                            elements={footprints.map(({id, metric}) => ({id: id, label: metric}))}
+                            value={footprint}
+                            setValue={setFootprint}
+                        ></SearchableSelect>
                     </div>
 
                     {/*partType: PartType*/}
@@ -119,8 +155,10 @@ export function CreateEditPart({open, part, cb}: {
                         <Label htmlFor="sheet-demo-username">PartType</Label>
                         <SearchableSelect
                             placeholder={"PartType"}
-                            elements={partTypes.map(({id, name}) => ({id: id, label: name}))}>
-                        </SearchableSelect>
+                            elements={partTypes.map(({id, name}) => ({id: id, label: name}))}
+                            value={partType}
+                            setValue={setPartType}
+                        ></SearchableSelect>
                     </div>
 
                     {/*manufacturer: Manufacturer*/}
@@ -128,8 +166,10 @@ export function CreateEditPart({open, part, cb}: {
                         <Label htmlFor="sheet-demo-username">Manufacturer</Label>
                         <SearchableSelect
                             placeholder={"Manufacturer"}
-                            elements={manufacturers.map(({id, name}) => ({id: id, label: name}))}>
-                        </SearchableSelect>
+                            elements={manufacturers.map(({id, name}) => ({id: id, label: name}))}
+                            value={manufacturer}
+                            setValue={setManufacturer}
+                        ></SearchableSelect>
                     </div>
 
                     {/*tray: Tray*/}
@@ -139,13 +179,15 @@ export function CreateEditPart({open, part, cb}: {
                             placeholder={"Tray"}
                             elements={shelfs.map(it => {
                                 return it.trays.map(({id, name}) => ({id: id, label: name}))
-                            }).flat()}>
-                        </SearchableSelect>
+                            }).flat()}
+                            value={tray}
+                            setValue={setTray}
+                        ></SearchableSelect>
                     </div>
                 </div>
 
                 <SheetFooter>
-                    <Button onClick={() => cb(part)} type="submit">Save changes</Button>
+                    <Button onClick={() => cb(buildPart())} type="submit">Save changes</Button>
                     <SheetClose asChild>
                         <Button onClick={() => cb(undefined)} variant="outline">Close</Button>
                     </SheetClose>
